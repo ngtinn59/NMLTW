783c426922f5f4ac3a1606c067a8deb7257d8ea1
ngtinn59 <tinb2004814@student.ctu.edu.vn>
2023-12-08 15:49:46 +0700

    update
    update

---
+++ JS/data.js
@@ -0,0 +1,269 @@
+
+const data = [
+    {
+        "name": "GST-B500GD-9A",
+        "price": "25000",
+        "image": "./images/GST-B500GD-9A-removebg-preview.png",
+        "note": ["1 phần dưa chua", "  1 Phần cơm", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-100BP-1A ",
+        "price": "29000",
+        "image": "./images/GA-100BP-1A_01.png",
+        "note": ["1 phần dưa chua", "  1 Phần cơm", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-2100-1A2",
+        "price": "27000",
+        "image": "./images/GA-2100-1A2.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "Dòng Neon Accent", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GAE-2100RC-1A",
+        "price": "30000",
+        "image": "./images/GAE-2100RC-1A.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "DÒNG GA-2100", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "MRG-B5000B-1 ",
+        "price": "35000",
+        "image": "./images/MRG-B5000B-1.png",
+        "note": ["MR-G", "MRG-B5000 Serie", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-2100-1A2",
+        "price": "40000",
+        "image": "./images/GA-2100-1A2.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "Dòng Neon Accent", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-2100-1A3",
+        "price": "25000",
+        "image": "./images/GA-2100-1A3.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "Dòng Neon Accent", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-2100-1A4",
+        "price": "50000",
+        "image": "./images/GA-2100-1A4.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "Dòng Neon Accent", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "GA-2200SKL-4A ",
+        "price": "65000",
+        "image": "./images/GA-2200SKL-4A.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ", "Dòng Sound Wave", "G-SOCK"],
+        "phanloai": "Cơm gà"
+    },
+    {
+        "name": "BLX-565-2 ",
+        "price": " 500000",
+        "image": "./images/BLX-565-2.png",
+        "note": ["BLX-565 Series  ", " ", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BLX-565S-2 ",
+        "price": "149000",
+        "image": "./images/BLX-565S-2.png",
+        "note": ["BLX-565 Series", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGA-310-7A2 ",
+        "price": "192000",
+        "image": "./images/BGA-310-7A2_01.png",
+        "note": ["BGA-310 Series", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGA-310C-1A",
+        "price": "195000",
+        "image": "./images/BGA-310C-1A_01.png",
+        "note": ["BGA-310 Series", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGD-565S-7",
+        "price": "200000",
+        "image": "./images/BGD-565S-7.png",
+        "note": ["Baby G", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGA-310C-3A        ",
+        "price": "192000",
+        "image": "./images/BGA-310C-3A_01.png",
+        "note": ["BGA-310 Series  ", " ", ""],
+        "phanloai": "Cơm gà đặc biệt"
+},
+    {
+        "name": "BA-130SP-4A",
+        "price": "230000",
+        "image": "./images/BA-130SP-4A_1.png",
+        "note": ["DÒNG BA-130 ", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGD-560WL-7",
+        "price": "250000",
+        "image": "./images/BGD-560WL-7.png",
+        "note": ["Dòng BGD-560        ", "", ""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+    {
+        "name": "BGA-275-5A        ",
+        "price": "350000",
+        "image": "./images/BGA-275-5A.png",
+        "note": ["Dòng BGA-275        ", "",""],
+        "phanloai": "Cơm gà đặc biệt"
+    },
+
+    {
+        "name": "MTG-B3000B-1A  ",
+        "price": "80000",
+        "image": "./images/MTG-B3000B-1A_JF.png",
+        "note": ["MTG-B3000 Series ", "MT-G ", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "MTG-B3000BD-1A ",
+        "price": "50000",
+        "image": "./images/MTG-B3000BD-1A_JF.png",
+        "note": ["MTG-B3000 Series ", "MT-G ", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GMA-S2200M-4A_01",
+        "price": "100000",
+        "image": "./images/GMA-S2200M-4A_01.png",
+        "note": ["G-Shock", "ĐỒNG HỒ KIM-SỐ ", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GA-B2100-2A ",
+        "price": "30000",
+        "image": "./images/GA-B2100-2A.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ ", "D&ograve;ng 2100 ", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GBD-200UU-9 ",
+        "price": "80000",
+        "image": "./images/GBD-200UU-9.png",
+        "note": ["GBD-200UU-9  ", "G-SQUAD", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GST-B500GD-9A        ",
+        "price": "120000",
+        "image": "./images/GST-B500GD-9A.png",
+        "note": ["GST-B500 Series ", "G-STEEL", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GA-B2100C-9A",
+        "price": "30000",
+        "image": "./images/GA-B2100C-9A.png",
+        "note": ["ĐỒNG HỒ KIM-SỐ  ", "D&ograve;ng 2100 ", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GLX-5600RT-9",
+        "price": "80000",
+        "image": "./images/GLX-5600RT-9.png        ",
+        "note": ["Dòng GLX-5600 ", "G-LIDE", ""],
+        "phanloai": "Món phụ"
+    },
+    {
+        "name": "GST-B500BD-1A",
+        "price": "400000",
+        "image": "./images/GST-B500BD-1A_01.png ",
+        "note": ["GST-B500GD-9A ", "GST-B500 Series        ", ""],
+        "phanloai": "Món phụ"
+    }
+]
+
+
+const dataKM = [
+    {
+        "name": "Đồng hồ treo tường ",
+        "price": "600000",
+        "image": "./images/IQ-05-1.png",
+        "note": ["", " ", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "10"
+    },
+    {
+        "name": "DQ-541-1R",
+        "price": "115000",
+        "image": "./images/DQ-541-1R.png",
+        "note": ["", " ", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "12"
+    },
+    {
+        "name": "MTP-B120GL-7AV",
+        "price": "175000",
+        "image": "./images/MTP-B120GL-7AV.png",
+        "note": ["", "", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "15"
+    },
+    {
+        "name": "MTP-B310BL-1AV",
+        "price": "60000",
+        "image": "./images/MTP-B310BL-1AV.png",
+        "note": ["", " ", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "20"
+    },
+    {
+        "name": "MTP-B125B-8AV        ",
+        "price": "50000",
+        "image": "./images/MTP-B125B-8AV.png",
+        "note": ["", "", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "50"
+    },
+    {
+        "name": "MTP-E600B-1B        ",
+        "price": "80000",
+        "image": "./images/MTP-E600B-1B.png",
+        "note": ["", " ", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "8"
+    },
+    {
+        "name": "LTP-VT02BL-2A        ",
+        "price": "90000",
+        "image": "./images/LTP-VT02BL-2AU.png" ,
+        "note": ["", "", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "5"
+    },
+    {
+        "name": "LWS-1200H-7A1V",
+        "price": "45000",
+        "image": "./images/LWS-1200H-7A1V.png",
+        "note": ["", "", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "20"
+    },
+    {
+        "name": "GST-B500BD-1A        ",
+        "price": "63000",
+        "image": "./images/GST-B500BD-1A_01.png",
+        "note": ["", "", ""],
+        "phanloai": "Khuyến mãi",
+        "discount": "25"
+    }
+]
